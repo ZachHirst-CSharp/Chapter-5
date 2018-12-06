c3a0b28d8ded6dd8fcb121637ff2b54df9d09e86
Zach <zach_hirst@live.com>
2018-12-06 13:10:32 -0600

    push

--- GuessingGame3/GuessingGame3/Program.cs
+++ GuessingGame3/GuessingGame3/Program.cs
@@ -11,6 +11,67 @@ namespace GuessingGame3
     {
         static void Main(string[] args)
         {
+            int min = 1;
+            int max = 11;
+            int againL = 1;
+            int againH = 10;
+            int count = 0;
+            string guessString;
+            int guess = 90000;
+            string result = null;
+
+            Random ranNumberGenerator = new Random();
+            int randomNumber;
+            randomNumber = ranNumberGenerator.Next(min, max);
+
+            while (guess != randomNumber)
+            {
+                WriteLine("Choose a number between 1-10. ");
+                guessString = ReadLine();
+                guess = Convert.ToInt32(guessString);
+
+                if (guess <= 10 && guess >= 1)
+                {
+                    if (randomNumber == guess)
+                    {
+                        result = " Congrats on the guess of " + randomNumber;
+                    }
+                    else if (guess < randomNumber)
+                    {
+                        result = "You guessed " + guess + " , too low. ";
+                        if (guess <= againL)
+                        {
+                            result = "What?! " + guess + " Why? SMH. ";
+                        }
+                        againL = guess;
+                    }
+                    else if (guess > randomNumber)
+                    {
+                        result = "You guessed to high ";
+                        if (guess >= againH)
+                        {
+                            result = "What?! " + guess + " Why? SMH. ";
+
+                        }
+                        againH = guess;
+                    }
+                    else if (guess < min || guess >= max)
+                    {
+                        result = " Out of bounds. ";
+                    }
+                    DisplayString(result);
+                }
+            }
+            
+        }
+        static void DisplayString(string result)
+        {
+            WriteLine(result);
+        }
+    }
+}
+
+/*
             Random randomgenerator = new Random();
 
             int userguess, randomnum, guessdummy;
@@ -58,6 +119,4 @@ namespace GuessingGame3
             } while (userguess != randomnum);
 
             WriteLine("Congrats you won. ");
-        }
-    }
-}
+            */
